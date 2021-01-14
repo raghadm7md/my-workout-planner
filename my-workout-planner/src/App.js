790c8bb0ec7afad99a97ee5af3df293d457e96fc
raghadm7md <raghad.m7md.1417@gmail.com>
2021-01-14 15:12:44 +0300

    add an item in selected category

--- my-workout-planner/src/App.js
+++ my-workout-planner/src/App.js
@@ -14,22 +14,22 @@ class App extends Component {
       Sets: "",
       Reps: "",
       Weight: "",
-      radio: "",
+      SelectedValue: "",
     };
     this.formSubmit = this.formSubmit.bind(this);
   }
   componentDidMount() {
-    // let API =`http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en`
-    //   axios.get(API)
-    //   .then((response) => {
-    //     console.log('RESPONSE: ', response);
-    //     console.log('DATA: ', response.data);
-    //     let newQoute=response.data.quoteText
-    //     this.setState({quote : newQoute })
-    //   })
-    //   .catch((err) => {
-    //     console.log('ERR: ', err);
-    //   });
+    let API =`http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en`
+      axios.get(API)
+      .then((response) => {
+        console.log('RESPONSE: ', response);
+        console.log('DATA: ', response.data);
+        let newQoute=response.data.quoteText
+        this.setState({quote : newQoute })
+      })
+      .catch((err) => {
+        console.log('ERR: ', err);
+      });
   }
   formSubmit = (event) => {
     event.preventDefault();
@@ -38,7 +38,7 @@ class App extends Component {
     let sets = this.state.Sets;
     let reps = this.state.Reps;
     let weight = this.state.Weight;
-    let radio = this.state.radio;
+    let Selected = this.state.SelectedValue;
     let NewData = this.state.exercise;
     console.log(NewData);
     const obj = {
@@ -48,39 +48,57 @@ class App extends Component {
       reps: reps,
       weight: weight,
     };
-    //###########################
-    if (radio === "Abs") {
-      console.log(radio);
+    if (Selected === "Abs") {
+      console.log(Selected);
       console.log(NewData);
       let abs = this.state.exercise.Abs;
-      abs.push(obj);
-      console.log(abs);
+      abs.unshift(obj);
       this.setState({ exercise: abs });
-      console.log("Dooonnnee!!!");
-    } else if (radio === "Arms") {
-    } else if (radio === "back") {
-    } else if (radio === "chest") {
-    } else if (radio === "legs") {
+    } else if (Selected === "Arms") {
+      console.log(Selected);
+      console.log(NewData);
+      let arms = this.state.exercise.Arms;
+      arms.unshift(obj);
+      this.setState({ exercise: arms });
+    } else if (Selected === "back") {
+      console.log(Selected);
+      console.log(NewData);
+      let back = this.state.exercise.back;
+      back.unshift(obj);
+      this.setState({ exercise: back });
+    } else if (Selected === "chest") {
+      console.log(Selected);
+      console.log(NewData);
+      let chest = this.state.exercise.chest;
+      chest.unshift(obj);
+      this.setState({ exercise: chest });
+    } else if (Selected === "legs") {
+      console.log(Selected);
+      console.log(NewData);
+      let legs = this.state.exercise.legs;
+      legs.unshift(obj);
+      this.setState({ exercise: legs });
     }
 
-    
-    console.log(NewData);
     this.setState({ exercise: NewData });
+
+    this.setState({ ExerciseName: "" });
+    this.setState({ Sets: "" });
+    this.setState({ Reps: "" });
+    this.setState({ Weight: "" });
+    this.setState({ SelectedValue: "" });
   };
   render() {
-    console.log(this.state.Body);
-    console.log(this.state.exercise);
-
     return (
       <div className="App">
         <h1>welcome to your fitness journey</h1>
-        {/* <button onClick={this.Getquote}> Quote of the day </button> */}
         <div>
           <form className="form" onSubmit={this.formSubmit}>
             Add your workout HERE!
             <div>
               <label>Exercise name: </label>
               <input
+                value={this.state.ExerciseName}
                 type="text"
                 onChange={(event) => {
                   this.setState({ ExerciseName: event.target.value });
@@ -88,6 +106,7 @@ class App extends Component {
               />
               <label>Number of Sets: </label>
               <input
+                value={this.state.Sets}
                 type="text"
                 onChange={(event) => {
                   this.setState({ Sets: event.target.value });
@@ -95,6 +114,7 @@ class App extends Component {
               />
               <label>Number of Reps: </label>
               <input
+                value={this.state.Reps}
                 type="text"
                 onChange={(event) => {
                   this.setState({ Reps: event.target.value });
@@ -102,61 +122,24 @@ class App extends Component {
               />
               <label>How much of weight? </label>
               <input
+                value={this.state.Weight}
                 type="text"
                 onChange={(event) => {
                   this.setState({ Weight: event.target.value });
                 }}
               />
             </div>
-            <div>
-              <input
-                type="radio"
-                value="Arms"
-                onChange={(event) => {
-                  this.setState({ radio: event.target.value });
-                }}
-              />
-              <label for="male"> It is for your Arms?</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                value="Abs"
-                onChange={(event) => {
-                  this.setState({ radio: event.target.value });
-                }}
-              />
-              <label for="male"> It is for your Abs ?</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                value="legs"
-                onChange={(event) => {
-                  this.setState({ radio: event.target.value });
-                }}
-              />
-              <label for="male"> It is for your Legs ?</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                value="back"
-                onChange={(event) => {
-                  this.setState({ radio: event.target.value });
-                }}
-              />
-              <label for="male"> It is for your Back ?</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                value="chest"
-                onChange={(event) => {
-                  this.setState({ radio: event.target.value });
-                }}
-              />
-              <label for="male"> It is for your Chest ?</label>
+            <div>    
+            <select value={this.state.SelectedValue} onChange={(event) => {
+                  this.setState({ SelectedValue: event.target.value });
+                }} >
+              <option value="" >Select here</option>
+              <option value="Arms" >Arms</option>
+              <option value="Abs" >Abs</option>
+              <option value="legs">legs</option>
+              <option value="back">back</option>
+              <option value="chest">chest</option>
+            </select>
             </div>
             <button type="submit">Submit</button>
           </form>
@@ -168,4 +151,3 @@ class App extends Component {
 }
 
 export default App;
-
